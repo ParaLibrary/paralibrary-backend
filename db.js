b5c962c2a24ba8a51f5565a8c41275acb37bbd99
Ben Jenkins <jenkinsb1@spu.edu>
2020-04-14 21:44:56 -0700

    Added delete route for users

--- db.js
+++ db.js
@@ -1,61 +1,70 @@
-var mysql = require('mysql2');
-var config = require('./config/db');
+var mysql = require("mysql2");
+var config = require("./config/db");
 
-var pool = mysql.createPool({
-  host     : config.host,
-  port     : config.port,
-  user     : config.username,
-  password : config.password,
-  database : config.database,
-  connectionLimit: 5
-}).promise();
+var pool = mysql
+  .createPool({
+    host: config.host,
+    port: config.port,
+    user: config.username,
+    password: config.password,
+    database: config.database,
+    connectionLimit: 5,
+  })
+  .promise();
 
-pool.query('SELECT 1+1')
-.then(() => {
-  console.log('Connected to db');
-})
-.catch((e) => {
-  console.error('Can\'t establish connection to the database\n' + e);
-})
+pool
+  .query("SELECT 1+1")
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((e) => {
+    console.error("Can't establish connection to the database\n" + e);
+  });
 
-var books = (function() {
+var books = (function () {
   return {
-    get: function(bookId) {
-      return pool.query('SELECT * FROM books WHERE id = ?', [ bookId ])
-      .then(([rows, fields]) => {
-        if(!rows || rows.length === 0) {
-          return null;
-        }
-        return rows[0];
-      })
+    get: function (bookId) {
+      return pool
+        .query("SELECT * FROM books WHERE id = ?", [bookId])
+        .then(([rows, fields]) => {
+          if (!rows || rows.length === 0) {
+            return null;
+          }
+          return rows[0];
+        });
     },
-    delete: function(bookId) {
-      return pool.query('DELETE FROM books WHERE id = ?', [ bookId ]);
-    }
-  }
+    delete: function (bookId) {
+      return pool.query("DELETE FROM books WHERE id = ?", [bookId]);
+    },
+  };
 })();
 
-var users = (function() {
+var users = (function () {
   return {
-    get: function(userId) {
-      return pool.query('SELECT * FROM users WHERE id = ?', [ userId ])
-      .then(([rows, fields]) => {
-        if(!rows || rows.length === 0) {
-          return null;
-        }
-        return rows[0];
-      })
+    get: function (userId) {
+      return pool
+        .query("SELECT * FROM users WHERE id = ?", [userId])
+        .then(([rows, fields]) => {
+          if (!rows || rows.length === 0) {
+            return null;
+          }
+          return rows[0];
+        });
     },
-    insertOrUpdate: function(user) {
-      return pool.query('INSERT INTO users VALUES (?,?,?) ON DUPLICATE KEY UPDATE display_name = VALUES(display_name)', [ user.id, user.display_name, user.name ])
+    insertOrUpdate: function (user) {
+      return pool.query(
+        "INSERT INTO users VALUES (?,?,?) ON DUPLICATE KEY UPDATE display_name = VALUES(display_name)",
+        [user.id, user.display_name, user.name]
+      );
     },
-    delete: function() {
-
-    }
-  }
+    delete: function (userId) {
+      return pool.query("DELETE FROM users WHERE id = ?", [userId]);
+      //"DROP USER IF EXISTS", [userId]   I had this earlier as well.
+    },
+  };
 })();
 
 module.exports = {
   books,
-  users
-}
+  users,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../db.js');
+var db = require("../db.js");
 
-router.route('/')
-  .get(function(req, res) {
+router
+  .route("/")
+  .get(function (req, res) {
     // Get all users
   })
-  .post(function(req, res){
+  .post(function (req, res) {});
 
-  })
-
-router.route('/:id')
+router
+  .route("/:id")
   .get(function (req, res) {
-    db.users.get(req.params.id)
-    .then(function(user) {
-      if(!user) {
+    db.users.get(req.params.id).then(function (user) {
+      if (!user) {
         res.statusCode = 404;
         return;
       }
       res.statusCode = 200;
       res.json(user);
-    })
+    });
   })
-  .put(function(req, res) {
+  .put(function (req, res) {
     let user = req.body;
-    db.users.insertOrUpdate(user)
-    .then((result) => {
-      if(result.something) { // TODO
+    db.users.insertOrUpdate(user).then((result) => {
+      if (result.something) {
+        // TODO
         res.statusCode = 404;
         return;
       }
       res.statusCode = 200;
-    })
+    });
   })
-  .delete(function(req, res) {
-      
+  .delete(function (req, res) {
+    db.users.delete(req.params.id).then(function (user) {
+      // call delete
+      //if (user) {   // if user still exists, send an error code
+      //  res.statusCode = 404;
+      //  return;
+      //}
+      res.statusCode = 200; //
+      //res.json(user);
+    });
   });
 
 module.exports = router;
