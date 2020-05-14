71d58248b75ffd94de4901d36106b8527b191812
Tait Weicht <taitweicht@icloud.com>
2020-05-14 11:15:11 -0700

    Add a variety of loan states to test UI on frontend

--- routes/loans.js
+++ routes/loans.js
@@ -32,6 +32,236 @@ const fakeLoan = {
   status: "pending",
 };
 
+const loanedToMe = [
+  {
+    id: "5",
+    book_id: "91",
+    status: "pending",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "Consider Phlebas",
+      author: "Iain M. Banks",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "6",
+    book_id: "73",
+    status: "accepted",
+    book: {
+      id: "73",
+      user_id: "1",
+      title: "Of Mice and Men",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "7",
+    book_id: "37",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "The Fifth Season",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    status: "loaned",
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "8",
+    book_id: "4",
+    status: "returned",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "The Name of This Book is Secret",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+];
+
+const loanedByMe = [
+  {
+    id: "1",
+    book_id: "2",
+    status: "pending",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "Dummy's Guide to Telekinesis",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "2",
+    book_id: "1",
+    status: "accepted",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "Third Class Superhero",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "3",
+    book_id: "3",
+    status: "loaned",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "Genius at Play",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+  {
+    id: "4",
+    book_id: "4",
+    status: "returned",
+    book: {
+      id: "1",
+      user_id: "1",
+      title: "Cryptonomicon",
+      author: "",
+      isbn: "",
+      summary: "",
+      visibility: "public",
+    },
+    owner: {
+      id: "1",
+      name: "Bob",
+    },
+    requester: {
+      id: "2",
+      name: "Sally",
+    },
+    owner_contact: "bob@bob.bob",
+    requester_contact: "sally@sally.sally",
+    accept_date: "",
+    request_date: "",
+    loan_start_date: "",
+    loan_end_date: "",
+  },
+];
+
 router
   .route("/")
   .get(function (req, res) {
@@ -43,11 +273,11 @@ router
   });
 
 router.route("/owner").get(function (req, res) {
-  res.status(200).json([fakeLoan, fakeLoan, fakeLoan]);
+  res.status(200).json(loanedByMe);
 });
 
 router.route("/requester").get(function (req, res) {
-  res.status(200).json([fakeLoan, fakeLoan]);
+  res.status(200).json(loanedToMe);
 });
 
 router
